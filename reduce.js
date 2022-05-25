7dc822d37838823ed1a8c9715127d5d49a9a6f60
Margarita Sighton <31641188+dev-eComm@users.noreply.github.com>
2022-05-25 13:25:08 -0700

    Add function vowelCount

--- reduce.js
+++ reduce.js
@@ -30,8 +30,22 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
-
+function vowelCount(str) {
+var vowels = "aeiou";
+  return str.split("").reduce(function (acc, next) {
+    if (vowels.indexOf(next.toLowerCase()) !== -1) {
+      if (next in acc) {
+        acc[next]++;
+      } else {
+        acc[next] = 1;
+      }
+    }
+    return acc;
+  }, {});
+}
+console.log(vowelCount("Elie"));
+console.log(vowelCount("Tim"));
+console.log(vowelCount("Margarita"));
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
 
