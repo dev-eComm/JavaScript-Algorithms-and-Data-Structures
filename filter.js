c9429b9f558e75be46a5f3603a04afc9ab957000
Margarita Sighton <31641188+dev-eComm@users.noreply.github.com>
2022-05-25 09:27:26 -0700

    Add 2 more functions

--- filter.js
+++ filter.js
@@ -70,7 +70,41 @@ console.log(
     true
   )
 );
+function removeVowels(str) {
+  var vowels = "aeiou";
+  return str
+    .toLowerCase()
+    .split("")
+    .filter(function (value) {
+      return vowels.indexOf(value) === -1;
+    })
+    .join("");
+}
 
+console.log(removeVowels("Elie"));
+console.log(removeVowels("TIM"));
+console.log(removeVowels("ZZZZZZ"));
+
+/*
+Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled (HINT - you can use map and fitler to double and then filter the odd numbers).
+
+Examples:
+    doubleOddNumbers([1,2,3,4,5]) // [2,6,10]
+    doubleOddNumbers([4,4,4,4,4]) // []
+*/
+
+function doubleOddNumbers(arr) {
+  return arr
+    .filter(function (val) {
+      return val % 2 !== 0;
+    })
+    .map(function (val) {
+      return val * 2;
+    });
+}
+
+console.log(doubleOddNumbers([1, 2, 3, 4, 5]));
+console.log(doubleOddNumbers([4, 4, 4, 4, 4]));
 var arr1 = [1, 2, 3];
 var arr2 = arr1.filter(function (value) {
   return value > 2;
